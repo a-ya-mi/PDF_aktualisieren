db471157ab0da10293e532627d5742e47a421fe4
a-ya-mi <fattiavantia@gmail.com>
2017-04-11 12:52:14 +0200

    Ein Beispiel - von Konsole Name aingeben, Datum von Rechner. Das alles in .txt speichern und (fehlt) in .pdf konvertieren, das alles in .zip speichern.

---
+++ src/einPDFerstellen.java
@@ -0,0 +1,21 @@
+/**
+ * Created by Aksana on 11.04.2017.
+ */
+import java.io.*;
+
+public class einPDFerstellen {
+
+    //kann mit iText gemacht werden
+
+        public einPDFerstellen (File file) throws Exception {
+            try {
+
+            }
+
+            catch (Exception e) {
+                e.toString();
+            }
+        }
+
+
+}
